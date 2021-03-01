aca43c10e9a99d44f58e588f736033db072d990b
lamonikim <77967939+lamonikim@users.noreply.github.com>
2021-03-01 20:14:32 +0600

    Added Unity Files

---
+++ Assets/BallCoding.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCoding : MonoBehaviour
+{
+    public float speed = 30;
+
+    void Start()
+    {
+        // Initial Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+}
